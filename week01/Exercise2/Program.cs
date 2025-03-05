572a04ceee75c27f42b4e882d281b6ae45087492
JosedLeonett <josed.95@gmail.com>
2025-03-05 20:38:51 -0300

    FINISH CSE 210: C# Programming Exercise 2: If Statements

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Please enter number of your grade percentage without '%' symbol.");
+        int gradePercentage = int.Parse(Console.ReadLine());
+        string letter = "";
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        int gradePercentageLastDigit = gradePercentage % 10;
+        string sign = "";
+
+        if (gradePercentageLastDigit >= 7 && gradePercentage <= 90 && gradePercentage >= 60)
+        {
+            sign = "+";
+        }
+        else if (gradePercentageLastDigit <= 3 && gradePercentage >= 60)
+        {
+            sign = "-";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You have passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("You have not pass the course. Be effort for the next time.");
+        }
     }
 }
