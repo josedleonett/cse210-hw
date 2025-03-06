2cefb23c6e9f7a9c66e8754f7dc8707582f43065
JosedLeonett <josed.95@gmail.com>
2025-03-05 21:12:08 -0300

    FINISH CSE 210: C# Programming Exercise 3: Loops

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random random = new Random();
+        int randomMagicNumber = random.Next(1, 101);
+        bool isGuessed = false;
+        int guessCount = 0;
+
+        do
+        {
+            Console.WriteLine($"What is the magic number? {randomMagicNumber}");
+
+            Console.Write("What is your guess? ");
+            int guess = int.Parse(Console.ReadLine());
+
+            if (guess == randomMagicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+                isGuessed = true;
+            }
+            else if (guess > randomMagicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+
+            guessCount++;
+
+            if (isGuessed)
+            {
+                Console.WriteLine($"You guessed the magic number in {guessCount} tries.");
+
+                Console.Write("Do you want to play again? (type 'yes' to keep playing) ");
+                string playAgain = Console.ReadLine();
+
+                if (playAgain == "yes")
+                {
+                    randomMagicNumber = random.Next(1, 101);
+                    guessCount = 0;
+                    isGuessed = false;
+                }
+            }
+
+        } while (!isGuessed);
+
+
     }
 }
