e96161537a46eac5354161d796700e4efd85738e
JosedLeonett <josed.95@gmail.com>
2025-03-07 19:29:39 -0300

    FINISH CSE 210: C# Programming Exercise 4: Lists and Generics

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+
+            if (number == 0)
+            {
+                break;
+            }
+
+            numbers.Add(number);
+        } while (true);
+
+        int numbersSum = 0;
+        foreach (int number in numbers)
+        {
+            numbersSum += number;
+        }
+        Console.WriteLine($"The sum is: {numbersSum}");
+
+        if (numbers.Count > 0)
+        {
+            double average = (double)numbersSum / numbers.Count;
+            Console.WriteLine($"The average is: {average}");
+        }
+
+        Console.WriteLine($"The largest number is: {numbers.Max()}");
+
+        Console.WriteLine($"The smallest positive number is: {numbers.Where(n => n > 0).Min()}");
+
+        Console.WriteLine("The sorted list is:");
+        numbers.Sort();
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
