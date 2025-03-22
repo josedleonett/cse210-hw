916eb4b86dc18cbea5e415a6dd8a2e8c907bcd93
JosedLeonett <josed.95@gmail.com>
2025-03-22 10:45:11 -0300

    feat(week03): Implement Fraction class with constructors and methods for fraction representation
    W03 Learning Activity: Encapsulation

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction firstConstructor = new Fraction();
+        Fraction secondConstructor = new Fraction(5);
+        Fraction thirdConstructor = new Fraction(3, 4);
+        Fraction fourthConstructor = new Fraction(1, 3);
+
+        Console.WriteLine(firstConstructor.GetFractionString());
+        Console.WriteLine(firstConstructor.GetDecimalValue());
+
+        Console.WriteLine(secondConstructor.GetFractionString());
+        Console.WriteLine(secondConstructor.GetDecimalValue());
+        
+        Console.WriteLine(thirdConstructor.GetFractionString());
+        Console.WriteLine(thirdConstructor.GetDecimalValue());
+
+        Console.WriteLine(fourthConstructor.GetFractionString());
+        Console.WriteLine(fourthConstructor.GetDecimalValue());
     }
 }
