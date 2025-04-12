7e8b09570e825475690b7c79e94823fdf8b53138
JosedLeonett <josed.95@gmail.com>
2025-04-11 23:03:26 -0300

    feat(week06): Implement Shape hierarchy with Circle, Rectangle, and Square classes, and update Program to display shape details
    W06 Learning Activity: Polymorphism

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Shape square = new Square("Red", 5);
+        Shape rectangle = new Rectangle("Blue", 7, 6);
+        Shape circle = new Circle("Green", 3);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
---
+++ week06/Shapes/Shape.cs
@@ -0,0 +1,21 @@
+public abstract class Shape
+{
+    public string _color;
+
+    protected Shape(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    public abstract double GetArea();
+}
